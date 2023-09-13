7dd5d98328583e82586877323525fb5c1b5d4467
moronibr <moroni.ramos@gmail.com>
2023-09-13 20:33:34 -0300

    Sim

---
+++ app/Listeners/NotifyClassCanceled.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ClassCanceled;
+use App\Mail\ClassCanceledMail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NotifyClassCanceled
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ClassCanceled $event): void
+    {
+        $members = $event->scheduledClass->members();
+
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+
+        $details = compact('className','classDateTime');
+
+        $members->each(function($user) use ($details){
+            Mail::to($user)->send(new ClassCanceledMail($details));
+        });
+    }
+}
